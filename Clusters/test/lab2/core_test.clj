9af594c44cdacaf875435247edbf020ee020d977
HannaBautsiuk <v.rednay@list.ru>
2014-12-02 05:47:02 -0430

    tests were added

--- Clusters/test/lab2/core_test.clj
+++ Clusters/test/lab2/core_test.clj
@@ -2,6 +2,24 @@
   (:use clojure.test
         lab2.core))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest getMethodTest
+  (testing "getMethod function doesn't work"
+    (is (= (lab2.core/getMethod "hamming") hamming))
+    (is (= (lab2.core/getMethod "euclidian") euclidian))
+    (is (nil? (lab2.core/getMethod "hammi")))))
+
+
+(deftest findMaxTest
+  (testing "find-max function doesn't work"
+    (is (=   {:dst 4.5} (lab2.core/find-max `({:dst 1.3 } {:dst 4.5 } {:dst 4.1 } {:dst 2.3}))))))
+
+
+
+(deftest hammingTest
+  (testing "hamming function doesn't work"
+    (is (=  2 (lab2.core/hamming `( 2 1 3 5) `( 2 5 6 5) )))))
+
+
+(deftest euclidianTest
+  (testing "euclidian function doesn't work"
+    (is (=  25 (lab2.core/euclidian `( 2 1 3 5) `( 2 5 6 5) )))))
